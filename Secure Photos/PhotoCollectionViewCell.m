6f7ead1a08bc5c4fee96ac9dfacc6faff480c7fb
xeaza <taylorwrightsanson@gmail.com>
2014-10-31 15:18:41 -0500

    Adding collection view cell and collection view controller

---
+++ Secure Photos/PhotoCollectionViewCell.m
@@ -0,0 +1,13 @@
+//
+//  PhotoCollectionViewCell.m
+//  Secure Photos
+//
+//  Created by Taylor Wright-Sanson on 10/31/14.
+//  Copyright (c) 2014 Taylor Wright-Sanson. All rights reserved.
+//
+
+#import "PhotoCollectionViewCell.h"
+
+@implementation PhotoCollectionViewCell
+
+@end
---
+++ Secure Photos/PhotosViewController.h
@@ -0,0 +1,13 @@
+//
+//  PhotosViewController.h
+//  Secure Photos
+//
+//  Created by Taylor Wright-Sanson on 10/31/14.
+//  Copyright (c) 2014 Taylor Wright-Sanson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PhotosViewController : UICollectionViewController
+
+@end
