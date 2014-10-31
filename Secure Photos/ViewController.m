6cea0beeac878dc79a742d1e7f74b181ecf37be7
xeaza <taylorwrightsanson@gmail.com>
2014-10-31 16:27:34 -0500

    Adding login and alertview methods

--- Secure Photos/ViewController.m
+++ Secure Photos/ViewController.m
@@ -27,6 +27,42 @@
     // Dispose of any resources that can be recreated.
 }
 
-- (IBAction)onLoginButtonPressed:(id)sender {
+- (IBAction)onLoginButtonPressed:(id)sender
+{
+    if (self.usernameTextField.text.length > 0 && self.passwordTextField.text.length > 0) {
+        NSString *password = [SSKeychain passwordForService:@"MyPhotos" account:self.usernameTextField.text];
+
+        if (password.length > 0) {
+            if ([self.passwordTextField.text isEqualToString:password]) {
+                [self performSegueWithIdentifier:@"photosViewController" sender:nil];
+
+            } else {
+                UIAlertView *alertView = [[UIAlertView alloc]initWithTitle:@"Error Login" message:@"Invalid username/password combination." delegate:nil cancelButtonTitle:@"OK" otherButtonTitles:nil];
+                [alertView show];
+            }
+
+        } else {
+            UIAlertView *alertView = [[UIAlertView alloc] initWithTitle:@"New Account" message:@"Do you want to create an account?" delegate:self cancelButtonTitle:@"Cancel" otherButtonTitles:@"OK", nil];
+            [alertView show];
+        }
+
+    } else {
+        UIAlertView *alertView = [[UIAlertView alloc] initWithTitle:@"Error Input" message:@"Username and/or password cannot be empty." delegate:nil cancelButtonTitle:@"OK" otherButtonTitles:nil];
+        [alertView show];
+    }
 }
+
+-(void)alertView:(UIAlertView *)alertView clickedButtonAtIndex:(NSInteger)buttonIndex{
+    switch (buttonIndex) {
+        case 0:
+            break;
+        case 1:
+            [self createAccount];
+            break;
+        default:
+            break;
+    }
+}
+
+
 @end
