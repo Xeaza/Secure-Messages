4bfbf168c2cfcbf213ddb661d17206597dfd8e54
xeaza <taylorwrightsanson@gmail.com>
2014-11-02 22:32:45 -0600

    Added pendingpartners class

---
+++ Secure Photos/PendingPartners.h
@@ -0,0 +1,14 @@
+//
+//  PendingPartners.h
+//  Secure Photos
+//
+//  Created by Taylor Wright-Sanson on 11/2/14.
+//  Copyright (c) 2014 Taylor Wright-Sanson. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <Parse/Parse.h>
+
+@interface PendingPartners : PFObject
+
+@end
---
+++ Secure Photos/PendingPartners.m
@@ -0,0 +1,13 @@
+//
+//  PendingPartners.m
+//  Secure Photos
+//
+//  Created by Taylor Wright-Sanson on 11/2/14.
+//  Copyright (c) 2014 Taylor Wright-Sanson. All rights reserved.
+//
+
+#import "PendingPartners.h"
+
+@implementation PendingPartners
+
+@end
